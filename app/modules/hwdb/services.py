498ed25338ce61778323e6ccc3b33022e5313222
MaxxGTI <maxx.gti@gmail.com>
2025-03-02 22:22:20 +0300

    HWDB. Tomsk IS IAC. Export data to xlsx file for load to new IS IAC (add missed code).

--- app/modules/hwdb/services.py
+++ app/modules/hwdb/services.py
@@ -27,3 +27,33 @@ class HwUnitsService(FilterMixin):
         hw_unit_dict = hw_unit.to_python()
 
         return hw_unit_dict
+
+    @classmethod
+    def get_by_invnum_n_serial(cls, invnum, serialnum):
+        if not invnum and not serialnum:
+            return None
+        query = HwDbModel.query
+        if invnum:
+            query = query.filter(HwDbModel.invnum == invnum)
+        if serialnum:
+            query = query.filter(HwDbModel.serialnum == serialnum)
+        return query.first()
+
+    @classmethod
+    def get_by_invnum_n_type(cls, invnum, type_id):
+        if not invnum:
+            return None
+        query = HwDbModel.query
+        query = query.filter(HwDbModel.invnum == invnum)
+        if type_id:
+            query = query.filter(HwDbModel.type_id == type_id)
+        return query.first()
+
+    @classmethod
+    def get_hw_tomsk_models_list(cls, filters=None):
+        query = HwTomskModels.query
+        query = query.order_by(HwTomskModels.id.asc())
+        query = cls.apply_filters(query, HwTomskModels, filters)
+        units = query.all()
+
+        return units
